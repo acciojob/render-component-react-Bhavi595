fdd9df63c1127119bd325492f498a97dfd778844
Bhavi yadav <129272547+Bhavi595@users.noreply.github.com>
2023-12-12 08:46:59 +0000

    version 1

--- src/components/App.js
+++ src/components/App.js
@@ -3,7 +3,11 @@ import '../styles/App.css';
 
 const App = () => {
   return (
-    <div id="main"></div>
+    <div id="main">
+     <p>
+      <h1>I am learning React. My life is getting better.</h1>
+     </p>
+    </div>
   )
 }
 
